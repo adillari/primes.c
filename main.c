17ea4660e381ba8a607831932b20983f2b6dc12f
Adil Lari <adillari@pm.me>
2025-09-01 23:58:14 -0500

    refactor

--- main.c
+++ main.c
@@ -1,109 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 #define FILENAME "primes.txt"
 
-int main(int argc, char* argv[])
+void validate_args(int argc)
 {
     if (argc != 2)
     {
-        printf("Usage: ./primes {int less than INT_MAX-1}\n");
-        return 1;
+        printf("Primes up to but not including {number}\n");
+        printf("Usage: ./primes {number}\n");
+        exit(1);
     }
+}
 
-    const int LIMIT = atoi(argv[1])-1;
+uint64_t* allocate_integers_array(uint64_t size)
+{
+    uint64_t* integers = malloc(sizeof(uint64_t) * size);
 
-    int *integers = malloc(sizeof(int) * LIMIT);
     if (integers == NULL)
     {
         perror("malloc failed\n");
-        return 1;
+        exit(1);
     }
 
-    // fill our array with integers starting from 2
-    for (int iterator = 0; iterator < LIMIT; iterator++)
+    return integers;
+}
+
+void fill_array_with_zero_to_limit(uint64_t* integers, uint64_t array_size)
+{
+    for (uint64_t index = 0; index < array_size; index++)
     {
-        integers[iterator] = iterator+2;
+        integers[index] = index;
     }
+}
 
-    // iterate through integers...
-    for (int iterator = 0; iterator < LIMIT; iterator++)
+void replace_composites_with_zero(uint64_t* integers, uint64_t array_size)
+{
+    integers[1] = 0;
+
+    for (uint64_t index = 2; index < array_size; index++)
     {
-        int integer = integers[iterator];
+        // if (index > array_size/2) break;
 
-        if (integer == 0) continue;
+        printf("filtering out multiples of %" PRIu64 "\n", index);
 
-        printf("filtering out multiples of %d\n", integer);
-
-        // and convert any multiples of integers to 0
-        int multiple = integer;
+        uint64_t multiple = index;
         while (true)
         {
-            multiple += integer;
+            multiple += index;
 
-            int* integer_to_check_ptr = &integers[multiple-2]; // -2 because 2 is at index 0, 3 at 1, etc
+            if (multiple >= array_size) break;
 
-            if (multiple > LIMIT+1) break;
-            if (*integer_to_check_ptr == 0) continue;
+            if (integers[multiple] == 0) continue;
 
-            printf("setting %d to 0\n", integers[multiple-2]);
-            *integer_to_check_ptr = 0;
+            printf("setting %" PRIu64 " to 0\n", multiple);
+
+            integers[index] = 0;
         }
     }
+}
 
-    // integers array should only be prime numbers now
-
-    // lets count how many primes exist in the array, and
-    // allocate a new smaller array just to house these primes
-
-    // count 'em
-    int num_primes = 0;
-    for (int iterator = 0; iterator < LIMIT; iterator++)
-    {
-        if (integers[iterator] != 0)
-            num_primes++;
-    }
-
-    // allocate it
-    int *primes = malloc(sizeof(int) * num_primes);
-
-    // copy primes over to our new smaller array
-    int integers_iterator = 0;
-    int primes_iterator = 0;
-    while (integers_iterator < LIMIT)
-    {
-        int prime = integers[integers_iterator];
-
-        if (prime != 0)
-        {
-            primes[primes_iterator] = prime;
-            primes_iterator++;
-        }
-
-        integers_iterator++;
-    }
-
-    free(integers);
-
+void save_primes_to_file(uint64_t* integers, uint64_t array_size)
+{
     printf("\nSaving primes to %s...\n", FILENAME);
 
     FILE *fp = fopen(FILENAME, "w");
+
     if (fp == NULL)
     {
         perror("Error opening file");
-        return 1;
+        exit(1);
     }
     else
     {
-        for (int iterator = 0; iterator < num_primes; iterator++)
+        for (uint64_t index = 0; index < array_size; index++)
         {
-            fprintf(fp, "%d\n", primes[iterator]);
+            if (integers[index] == 0) continue;
+
+            fprintf(fp, "%d\n", integers[index]);
         }
     }
 
     printf("\nSaved primes to %s...\n", FILENAME);
+}
 
-    free(primes);
+
+int main(int argc, char* argv[])
+{
+    validate_args(argc);
+
+    const uint64_t LIMIT = strtoull(argv[1], NULL, 10);
+
+    uint64_t* integers = allocate_integers_array(LIMIT);
+
+    fill_array_with_zero_to_limit(integers, LIMIT);
+
+    replace_composites_with_zero(integers, LIMIT);
+
+    save_primes_to_file(integers, LIMIT);
+
+    free(integers);
 
     return 0;
 }
